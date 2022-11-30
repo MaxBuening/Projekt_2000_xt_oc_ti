933d85ab2aab7cbd506d3475e28a55df625da2b8
MaxBuening <buningmax@rocketmail.com>
2022-11-30 14:25:16 +0100

    First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="World" src="../assets/Vue_JS_RAM.png">
+    <HelloWorld msg="Hello World"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
