86047dda11c2bf7ca12c82555285e9767a94af1a
KilianRuhfus <kilian.ruhfus@gmail.com>
2023-01-02 15:05:16 +0100

    Home View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="World" src="../assets/Vue_JS_RAM.png">
-    <HelloWorld msg="Hello World"/>
+  <div class="home" style="align-content: center">
+    <img src="../assets/Projekt2000GrosseLogo.png" style="width: 1500px; padding-top: 15%">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
