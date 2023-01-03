1f4ccd1962b2efc7b6c1e4b5d0b414fe930da20b
SynCr0Nic3 <buningmax@rocketmail.com>
2023-01-04 00:33:41 +0100

    versucht einen test zum laufen zu bringen aber jest fehler "  SyntaxError: Cannot use import statement outside a module "

---
+++ tests/unit/userViewTest.spec.js
@@ -0,0 +1,14 @@
+import UserView from "@/views/UserView";
+import {mount} from "@vue/test-utils";
+import User from "@/components/User";
+
+
+ describe ("Testing UserView.vue", () => {
+
+     it ("should have user component",  () => {
+        const wrapper = mount(UserView)
+
+        const user = wrapper.findComponent(User)
+        expect(user.exists()).toBeTruthy()
+     });
+})
